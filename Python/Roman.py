2c1f8ea39bde668cb25b97abcd0f1faa09a32101
Tagle-Azca <gtaglea@hotmail.com>
2024-04-26 14:17:12 -0600

    roman to integrer py and java

--- Python/Roman.py
+++ Python/Roman.py
@@ -1,11 +1,17 @@
-def roman():
-    roman_numerals = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
-    nums = [1, 5, 10, 50, 100, 500, 1000]
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        ans = 0
+        roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
-    roman_dict = dict(zip(roman_numerals, nums))
+        for i in range(len(s) - 1):
+            if roman[s[i]] < roman[s[i + 1]]:
+                ans -= roman[s[i]]
+            else:
+                ans += roman[s[i]]
 
-    print(roman_numerals)
-    print(nums)  # print
+        return ans + roman[s[-1]]
 
-
-roman()
+# Ejemplo de uso:
+sol = Solution()
+result = sol.romanToInt("MCMXCIV")
+print(result)  # Debería imprimir 1994

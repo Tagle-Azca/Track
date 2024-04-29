6a3f19edba07a574459ef3b550a2f5cb6ccee5c2
Tagle-Azca <gtaglea@hotmail.com>
2024-04-28 21:25:39 -0600

    inicio de proyecto de terminal de banco

---
+++ C/Proyecto/Transacciones.h
@@ -0,0 +1,14 @@
+// transacciones.h
+
+#ifndef TRANSACCIONES_H
+#define TRANSACCIONES_H
+
+void solicitarCuentaOrigen();
+void solicitarCuentaDestino();
+void solicitarNombreDestino();
+void solicitarBancoDestino();
+void solicitarMonto();
+void confirmarTransferencia();
+void mostrarResultado();
+
+#endif // TRANSACCIONES_H

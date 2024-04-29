a8f574ef2e0d9a6a73c10e50f0174a5cae1093ef
Tagle-Azca <gtaglea@hotmail.com>
2024-04-29 02:10:10 -0600

    Implementacion de recoleccion de datos de tarjeta local y destino, asi como el nombre, con sus procesos para evitar errores

--- C/Proyecto/Depositos/NombreDestino.c
+++ C/Proyecto/Depositos/NombreDestino.c
@@ -6,13 +6,19 @@
 int NombreDestino()
 {
     char nombre[100];
+
+    // Limpiar el buffer de entrada antes de leer el nombre
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF)
+        ; // Descarta caracteres hasta el final de la línea o EOF
+
     printf("Ingrese el nombre del destinatario: ");
     fgets(nombre, sizeof(nombre), stdin);
     nombre[strcspn(nombre, "\n")] = 0; // Elimina el salto de línea
 
     int longitud = strlen(nombre);
     if (longitud < 2 || longitud > 30)
-    {
+    { // Asegúrate de que la longitud mínima sea mayor que 0
         return 0;
     }
     for (int i = 0; i < longitud; i++)

82dc909d18f2731e7033d1427fa1a72cd82396de
Tagle-Azca <gtaglea@hotmail.com>
2024-05-08 19:02:08 -0600

    clone testing

--- Python/Palindrome.py
+++
@@ -1,9 +0,0 @@
-def palindrome(word):
-    for i in range(0, int(len(word)/2)):
-        if word[i] != word[len(word)-i-1]:
-            return False
-        return True
-
-
-word = input("Escriba una palabra: ").lower()
-print(word, 'es un paliondromo?', palindrome(word))
